7464c8952120093e1a4f7bbd69a8a4ab2c06c558
Will Frost <williamf1245@gmail.com>
2020-07-23 11:39:12 +0200

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by William Frost on 2020/07/23.
+//  Copyright © 2020 Frostech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
